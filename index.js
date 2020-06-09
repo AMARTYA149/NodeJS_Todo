ecbc652b2e42e2698c3417db9abfbd49f28e2064
Amartya Aishwarya <amartya970914@gmail.com>
2020-06-09 13:58:10 +0530

    todo desc passing to mongodb successfully

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const path = require('path');
 const port = 8000;
 const app = express();
 
+//setting up mongo
+const db = require('./config/mongoose');
+const Todo = require('./models/tododb');
+
 //setup the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -12,6 +16,21 @@ app.use(express.static('assets'));
 app.get('/', function(request, response){
     return response.render('home', {
         title: "TODO App"
+    });
+});
+
+app.post('/create-todo', function(request, response){
+    Todo.create({
+        todoDesc: request.body.task_name
+        // category: request.body.category
+        // date: request.body.date
+    }, function(err, newTodo){
+        if(err){
+            console.log('error in creating todo!');
+            return;
+        }
+        console.log('***********', newTodo);
+        return response.redirect('back');
     });
 });
 
